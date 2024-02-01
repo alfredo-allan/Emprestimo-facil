a9492ad10c56615331c760bc196e67effefbca39
allan-mecanico <allan-mecanico@outlook.com>
2024-01-31 22:00:55 -0300

    finalização-footer

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-window.addEventListener("scroll", function() {
-  var button = document.getElementById("botao");
-  var rect = button.parentNode.getBoundingClientRect();
-  button.style.top = rect.top + "px";
+document.getElementById("botao").addEventListener("click", function() {
+  window.location.href = "https://api.whatsapp.com/send?phone=5511931311054"; // Coloque o link da API do WhatsApp aqui
 });
 
+window.onscroll = function() {
+  var button = document.getElementById("whatsapp-button");
+  
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    button.style.opacity = "1";
+  } else {
+    button.style.opacity = "0";
+  }
+};
 
